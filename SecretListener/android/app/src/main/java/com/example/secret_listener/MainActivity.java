377a183382f2337255a070c16bc4b3a388e3e0e0
Yusef <yusef@Air-di-Yusef.broadband>
2024-07-20 07:32:16 +0200

    Progetto caricato con successo

---
+++ SecretListener/android/app/src/main/java/com/example/secret_listener/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.secret_listener;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ SecretListener/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Secret Listener v.1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Secret Listener'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FilePickerResult? result;
+  AudioPlayer audioPlayer = AudioPlayer();
+  double playBackRate = 1;
+  IconData icon = Icons.play_arrow;
+  bool isPlay = false;
+
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        isPlay = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body:
+      Center(
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network('https://static.vecteezy.com/system/resources/previews/023/986/631/original/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png', height:200, width:200),
+                const Text(
+                  'Secret Listener è l\'app che ti permette di ascoltare l\'ultimo vocale su WhatsApp',
+                  textAlign: TextAlign.center,
+                ),
+                result == null ? const Text('Seleziona Audio...') : Text('Selezionato : '+result!.files.single.name),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: playAudio,
+                      child: isPlay == false ? Icon(icon, color: Colors.green) : Icon(Icons.pause, color: Colors.blue)
+                  ),
+                  TextButton(onPressed: upDownVelocity, child: Text("x$playBackRate")),
+                ],
+              )
+              ],
+            ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: filePicker,
+        child: const Icon(Icons.file_open),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void filePicker() async{
+    result = await FilePicker.platform.pickFiles();
+    setState(() {});
+  }
+
+  void playAudio() {
+    if(result != null){
+      setState(() {
+        isPlay = true;
+      });
+      audioPlayer.play(DeviceFileSource(result!.files.single.path!));
+    }
+
+  }
+
+  void upDownVelocity() {
+    playBackRate = playBackRate == 1 ? 2 : 1;
+    audioPlayer.setPlaybackRate(playBackRate);
+    setState(() {
+    });
+  }
+}
